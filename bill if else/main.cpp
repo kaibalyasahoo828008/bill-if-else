8e5d569341639e5768d1f27688373dc1302ba368
kaibalya sahoo <120CR0406@nitrkl.ac.in>
2021-03-15 14:27:35 +0530

    Initial Commit

---
+++ bill if else/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  bill if else
+//
+//  Created by KAIBALYA SAHOO on 24/12/1942 Saka.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
